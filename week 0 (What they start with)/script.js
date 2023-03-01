89772557e05539c06582250d171c3b4736eba6bd
studio255coding2 <123024166+studio255coding2@users.noreply.github.com>
2023-03-01 02:08:40 +0000

    gravity

--- week 0 (What they start with)/script.js
+++ week 0 (What they start with)/script.js
@@ -12,19 +12,34 @@ document.body.appendChild(renderer.domElement)
 var geometry = new THREE.BoxGeometry(1,1,1)
 var material = new THREE.MeshLambertMaterial({color: "turqise"})
 var cube1 = new THREE.Mesh(geometry, material)
-cube1.rotation.z = 45;
+//cube1.rotation.z = 45;
 scene.add(cube1)
+
+var geometry = new THREE.BoxGeometry(30, 1, 30); 
+var material = new THREE.MeshLambertMaterial({color: 0xFFCC00}); 
+var ground = new THREE.Mesh(geometry, material);
+ground.position.y = -1
+scene.add(ground);
 
 var light = new THREE.PointLight(new THREE.Color("white"), 1, 500)
 light.position.set(10,10,10)
 scene.add(light)
 
+var raycaster = new THREE.Raycaster();
+var downDirection = new THREE.Vector3( 0, -1, 0)
+var gravity = 0.2
 var render = function() {
-      requestAnimationFrame(render);
-      renderer.render(scene, camera); 
-  }
-  
-  render();
+  raycaster.set(camera.position, downDirection);
+          const intersects = raycaster.intersectObjects(scene.children);
+          if(intersects.length>0) {
+            if(intersects[0].distance>1) {
+                camera.position.y-=gravity;
+            }
+          }
+    requestAnimationFrame(render);
+    renderer.render(scene, camera); 
+}
+render()
 
 
 
@@ -46,6 +61,8 @@ document.body.onmousemove = function(evt) {
     if(evt.keyCode==38||evt.keyCode==87) { 
       var direction = new THREE.Vector3();
       camera.getWorldDirection( direction );
+      camera.position.add( direction );
+
       camera.position.add( direction );
     }
     if(evt.keyCode==40||evt.keyCode==83) { 
